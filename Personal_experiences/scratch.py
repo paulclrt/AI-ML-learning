b60402fc04c1b71ab53094b1731a16601611ed57
pauclrt <p4ul.claret@gmail.com>
2024-11-11 14:06:54 -0600

    backprop added

--- Personal_experiences/scratch.py
+++ Personal_experiences/scratch.py
@@ -6,64 +6,79 @@ import numpy as np
 
 
 
-class NeuralNet():
+import numpy as np
 
-    def __init__(self, number_of_input, input_values, activation_func):
-        # no input validation for now because this is just to understand the basics, not deploy
-        self.number_of_input = number_of_input # number of nodes in input layer
+class NeuralNet:
+    def __init__(self, activation_func):
         self.activation_func = activation_func
-        self.layers = [{"n_of_nodes": number_of_input, "values": input_values}]
-
-
+        self.layers = []
+        self.activations = []  # Store activations for each layer during forward pass
 
     def activation_function(self, x):
         if self.activation_func == "relu":
             return np.maximum(0, x)
         else:
-            return np.maximum(0, x) # for now just because too lazy to implement another
+            return np.maximum(0, x)  # Placeholder for other activations
+
+    def activation_derivative(self, x):
+        if self.activation_func == "relu":
+            return np.where(x > 0, 1, 0)
+        else:
+            return 1  # Placeholder for other activation derivatives
 
     def loss_function(self, output, expected_output):
-        loss = ((np.sum(output-expected_output))**2)/2
-        return loss
+        return ((np.sum(output - expected_output)) ** 2) / 2
 
+    def add_input_layer(self, input_values):
+        n_input_nodes = len(input_values)
+        self.layers.append({"n_of_nodes": n_input_nodes, "values": input_values})
 
     def add_hidden_layer(self, number_of_nodes):
-        """take the last layer number of nodes. make a matrice of size (number_of_nodes, number_of_previous_node)"""
-        if len(self.layers) == 1:
-            # only input layer has been added so ge the previous number of nodes from self.numer_of_ninput
-            previous_number_of_nodes = self.number_of_input
-        else:
-            #get the layer object number of nodes
-            previous_number_of_nodes = self.layers[-1]['n_of_nodes']
-
-        weights = np.random.randint(100, size=(number_of_nodes, previous_number_of_nodes))/1000
-        biases = np.random.randint(100, size=(number_of_nodes, ))/100
+        previous_number_of_nodes = self.layers[-1]['n_of_nodes']
+        weights = np.random.rand(number_of_nodes, previous_number_of_nodes) / 100
+        biases = np.random.rand(number_of_nodes) / 100
         self.layers.append({"n_of_nodes": number_of_nodes, "weights": weights, "biases": biases})
 
-
     def forward_prop(self):
-
         activation = self.layers[0]["values"]
+        self.activations = [activation]  # Reset activations for a new pass
 
-        # Propagate through each layer
-        for i in range(1, len(self.layers)):  # Skip input layer, start from first hidden layer
+        for i in range(1, len(self.layers)):
             layer = self.layers[i]
-
-            # Perform dot product (input * weights) + biases
-            dot_prod = np.dot(activation, layer["weights"].T) + layer["biases"].T
-
-            # Apply activation function (ReLU)
+            dot_prod = np.dot(activation, layer["weights"].T) + layer["biases"]
             activation = self.activation_function(dot_prod)
+            self.activations.append(activation)
 
-        return activation  # This will be the output of the network
-    
+        return activation
+
+    def backprop(self, expected_output, learning_rate=0.01):
+        error = self.activations[-1] - expected_output
+        
+        for i in reversed(range(1, len(self.layers))):
+            layer = self.layers[i]
+            activation_prev = self.activations[i-1]
+            derivative = self.activation_derivative(self.activations[i])
+
+            dz = error * derivative
+            dW = np.dot(dz[:, np.newaxis], activation_prev[np.newaxis, :])
+            db = np.sum(dz, axis=0)
+
+            layer["weights"] -= learning_rate * dW
+            layer["biases"] -= learning_rate * db
+
+            error = np.dot(dz, layer["weights"])
+
+    def train(self, X, Y, epochs=100, learning_rate=0.01):
+        for epoch in range(epochs):
+            self.layers[0]["values"] = X  # Set input layer values
+            output = self.forward_prop()
+            self.backprop(Y, learning_rate)
+            loss = self.loss_function(output, Y)
+            print(f"Epoch {epoch+1}/{epochs}, Loss: {loss:.4f}")
 
 
-
-
-nn = NeuralNet(number_of_input=3, input_values=np.array([1, 0, 1]), activation_func="relu")
+nn = NeuralNet(number_of_input=3, activation_func="relu")
 nn.add_hidden_layer(4)
 nn.add_hidden_layer(2)  # Output layer with 2 nodes (binary classification)
 
-output = nn.forward_prop()  # Perform forward propagation
-print("Output:", output)
+nn.train()
